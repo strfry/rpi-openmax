55d7ea08fe0e3d7ee393eab77abf903f9ad9dd85
Jonathan <mail@strfry.org>
2016-12-16 15:22:33 +0000

    New working state...

--- yuvvideo.c
+++ yuvvideo.c
@@ -4,14 +4,37 @@
 
 #include <assert.h>
 
+#include <pthread.h>
+
 //#include <bcm_host.h>
 #include <IL/OMX_Broadcom.h>
+
+#ifndef BARESIP
+#define info printf
+#define warn printf
+#define error printf
+#endif
 
 
 static int EventHandler(OMX_HANDLETYPE hComponent, void* pAppData, OMX_EVENTTYPE eEvent,
         OMX_U32 nData1, OMX_U32 nData2, void* pEventData)
 {	
-	puts("OMX EventHandler");
+	switch (eEvent) {
+		case OMX_EventCmdComplete:
+			//info("omx.EventHandler: Previous command completed\n");
+			printf("!!!omx.EventHandler: Previous command completed\n"
+				"d1=%x\td2=%x\teventData=%p\tappdata=%p\n",
+				nData1, nData2, pEventData, pAppData);
+			// Set a semaphor, put event into mt queue?
+			break;
+		case OMX_EventError:
+			warn("!!!omx.EventHandler: Error event type data1=%x\tdata2=%x\n", nData1, nData2);
+			break;
+		default:
+			warn("!!!omx.EventHandler: Unknown event type %d\tdata1=%x\tdata2=%x\n", eEvent, nData1, nData2);
+			return -1;
+			break;
+	}
 	return 0;
 }
 		
@@ -37,13 +60,19 @@ static struct OMX_CALLBACKTYPE callbacks = {
 
 // GLOBAL BUFFER
 
-OMX_BUFFERHEADERTYPE* bufhdr;
-
-OMX_HANDLETYPE video_render;
+static OMX_BUFFERHEADERTYPE* bufhdr;
+static OMX_HANDLETYPE video_render;
+static pthread_mutex_t omx_mutex;
   
-int omx_alloc_render(void)
+void omx_bcm_init(void)  
 {
    bcm_host_init();
+}
+  
+int omx_alloc_render(void)
+{   
+   pthread_mutex_init(&omx_mutex, 0);
+   pthread_mutex_lock(&omx_mutex);
    
    if(OMX_Init() != OMX_ErrorNone)
    {
@@ -55,9 +84,9 @@ int omx_alloc_render(void)
    int status = 0;
    assert(status == 0);
    
-	OMX_ERRORTYPE error = OMX_GetHandle(&video_render, "OMX.broadcom.video_render", 0x1337, &callbacks);
+	OMX_ERRORTYPE err = OMX_GetHandle(&video_render, "OMX.broadcom.video_render", 0x1337, &callbacks);
 		
-	if (error) {
+	if (err) {
 		puts("OMX_GetHandle error");
 		return 0;
 	}
@@ -76,39 +105,58 @@ int omx_alloc_render(void)
 	format.eCompressionFormat = OMX_VIDEO_CodingUnused;
 	format.eColorFormat = OMX_COLOR_FormatYUV420PackedPlanar;
    
-	puts("Set Format parameters");
-	if (OMX_SetParameter(video_render, OMX_IndexParamVideoPortFormat, &format) != OMX_ErrorNone) {
-		puts("fucking error setting format on video_render");
-		exit(-23);
-	}
+	//puts("Set Format parameters");
+	//if (OMX_SetParameter(video_render, OMX_IndexParamVideoPortFormat, &format) != OMX_ErrorNone) {
+	//	puts("fucking error setting format on video_render");
+	//	exit(-23);
+	//}
 	
-	int portIndex = 90;
-	error = OMX_SendCommand(video_render, OMX_CommandPortEnable, portIndex, NULL);
-	assert(error == OMX_ErrorNone);	
+	/*
+	puts("sleep(1)");
+	sleep(1);
+	int state;
+	puts("to idle state; sleep(1)");
+	OMX_SendCommand(video_render, OMX_CommandStateSet, OMX_StateIdle, NULL);
+	sleep(1);
+	err = OMX_GetState(video_render, &state);
+	if (err != OMX_ErrorNone || state != OMX_StateIdle) {
+		puts("first state ever: not in idle state");
+	    return -1;
+	}*/
+	
+	//puts("enable port 90");
+	//int portIndex = 90;
+	//err = OMX_SendCommand(video_render, OMX_CommandPortEnable, portIndex, NULL);
+	//assert(err == OMX_ErrorNone);	
 	
 	
+	/*
 	int state;
 	OMX_SendCommand(video_render, OMX_CommandStateSet, OMX_StateIdle, NULL);
 	puts("to idle state");
 	sleep(1);
-	error = OMX_GetState(video_render, &state);
+	err = OMX_GetState(video_render, &state);
 	if (error != OMX_ErrorNone || state != OMX_StateIdle) {
-		puts("video_render not in idle state");
+		puts("omx_alloc_render: video_render not in idle state");
 	    return -1;
 	}
 	puts("to execute state");
 	OMX_SendCommand(video_render, OMX_CommandStateSet, OMX_StateExecuting, NULL);
-	sleep(1);
-	error = OMX_GetState(video_render, &state);
-	if (error != OMX_ErrorNone || state != OMX_StateExecuting) {
+	//sleep(1);
+	err = OMX_GetState(video_render, &state);
+	if (err != OMX_ErrorNone || state != OMX_StateExecuting) {
 		puts("video_render not in executing state");
 	    return -1;
-	}
+	}*/
 	
+	pthread_mutex_unlock(&omx_mutex);
 	return 0;
 }
 
 int omx_update_size(int width, int height) {
+	
+   pthread_mutex_lock(&omx_mutex);
+	info("omx_update_size %d %d\n", width, height);
 	
    // Where is the resolution?? -> In the port definition
 
@@ -123,15 +171,15 @@ int omx_update_size(int width, int height) {
 	portdef.nPortIndex = 90;
 	// work out buffer requirements, check port is in the right state
 
-	int error = OMX_GetParameter(video_render, OMX_IndexParamPortDefinition, &portdef);
+	int err = OMX_GetParameter(video_render, OMX_IndexParamPortDefinition, &portdef);
 	
 	portdef.format.video.nFrameWidth = 320;
 	portdef.format.video.nFrameHeight = 240;
 	portdef.format.video.nStride = 320;
 	portdef.format.video.nSliceHeight = 240;
 	
-	error = OMX_SetParameter(video_render, OMX_IndexParamPortDefinition, &portdef);
-	error = OMX_GetParameter(video_render, OMX_IndexParamPortDefinition, &portdef);
+	err = OMX_SetParameter(video_render, OMX_IndexParamPortDefinition, &portdef);
+	err = OMX_GetParameter(video_render, OMX_IndexParamPortDefinition, &portdef);
 	
 	puts("set port definition parameter");
 	
@@ -139,6 +187,7 @@ int omx_update_size(int width, int height) {
 	
 	OMX_SendCommand(video_render, OMX_CommandStateSet, OMX_StateIdle, NULL);
 	puts("to idle state");
+	usleep(1000);
 	
 	int i;
 		for (i = 0; i < portdef.nBufferCountActual; i++) {
@@ -150,8 +199,8 @@ int omx_update_size(int width, int height) {
 			
 		//	OMX_BUFFERHEADERTYPE* bufhdr;
 		const int portIndex = 90;
-			int error = OMX_UseBuffer(video_render, &bufhdr, portIndex, 0x777, portdef.nBufferSize, buf);
-			assert(error == OMX_ErrorNone);
+			int err = OMX_UseBuffer(video_render, &bufhdr, portIndex, 0x777, portdef.nBufferSize, buf);
+			assert(err == OMX_ErrorNone);
 		} 
 		
 	   if(i != portdef.nBufferCountActual) {
@@ -159,30 +208,41 @@ int omx_update_size(int width, int height) {
 		}
 	
 	
+	/*
 	int state;
 	puts("to execute state");
 	OMX_SendCommand(video_render, OMX_CommandStateSet, OMX_StateExecuting, NULL);
-	sleep(1);
-	error = OMX_GetState(video_render, &state);
-	if (error != OMX_ErrorNone || state != OMX_StateExecuting) {
+	//sleep(1);
+	err = OMX_GetState(video_render, &state);
+	if (err != OMX_ErrorNone || state != OMX_StateExecuting) {
 		puts("video_render not in executing state");
 	    return -1;
 	}
 	//sleep(1);
+	* */
+	
+   pthread_mutex_unlock(&omx_mutex);
 	
 }
 
 int omx_send_frame(void* data, uint32_t len)
 {
-	/*
+	
+	pthread_mutex_lock(&omx_mutex);
+   
 	int state;
 	// Verify Executing state of video_render component
-	int error = OMX_GetState(video_render, &state);
-	if (error != OMX_ErrorNone || state != OMX_StateExecuting) {
+	int err = OMX_GetState(video_render, &state);
+	//printf("Current State of video_render: %d\n", state);   
+	
+	if (err != OMX_ErrorNone || state != OMX_StateExecuting) {
 		puts("video_render not in executing state");
-	    return -1;
+		OMX_SendCommand(video_render, OMX_CommandStateSet, OMX_StateExecuting, NULL);
+		puts("OMX_SendCommand StateExecuting, sleep(1)\n");
+		sleep(1);
+	    //return -1;
 	}
-	* */
+	
 	
 	
 	static int first_packet = 1;
@@ -237,6 +297,8 @@ int omx_send_frame(void* data, uint32_t len)
    //OMX_Deinit();
 
    //ilclient_destroy(client);
+   
+   pthread_mutex_unlock(&omx_mutex);
    return status;   
 }
 
@@ -254,7 +316,7 @@ int main (int argc, char **argv)
       return -2;
   }
       
-      
+   omx_bcm_init();   
    omx_alloc_render();
    omx_update_size(320, 240);
    
